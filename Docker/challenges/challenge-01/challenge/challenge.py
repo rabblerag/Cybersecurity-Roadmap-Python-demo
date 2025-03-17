9ad91c1adeb599d0a4ec8baf975b8fa62d66d0cc
nikolasfil <nikolas13in@gmail.com>
2025-03-17 21:57:46 +0200

    added the first challenge

---
+++ Docker/challenges/challenge-01/challenge/challenge.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import sys
+
+
+class Challenge:
+    def __init__(self):
+        self.path = Path(__file__).parent
+        self.open_flag()
+
+    def main(self):
+        print("Hello world")
+        print(self.flag)
+        
+
+
+    def open_flag(self):
+        with open(Path(self.path, "flag.txt")) as file:
+            self.flag = file.read()
+
+
+if __name__ == "__main__":
+    challenge = Challenge()
+    challenge.main()
