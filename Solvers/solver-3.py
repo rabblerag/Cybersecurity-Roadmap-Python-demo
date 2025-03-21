25a0a956c4e66d5cbfd9f05216cba25e7cdfd2cb
nikolasfil <nikolas13in@gmail.com>
2025-03-21 09:14:04 +0200

    Changed the port and remove one receive line for the solver to work correctly

--- Solvers/solver-3.py
+++ Solvers/solver-3.py
@@ -4,15 +4,15 @@ from string import digits
 from itertools import product
 
 
-alphabet = digits + 'abcdef'
+alphabet = digits + "abcdef"
 
-chall = remote('localhost', 4024)
+chall = remote("localhost", 4075)
 print(chall.recvline().decode())
 print(chall.recvline().decode())
-print(chall.recvline().decode())
+# print(chall.recvline().decode())
 
 for candidate in product(alphabet, repeat=4):
-    chall.sendline(''.join(candidate).encode())
+    chall.sendline("".join(candidate).encode())
     response = chall.recvline()
     if b"correct" in response:
         print(response)
@@ -22,7 +22,7 @@ for candidate in product(alphabet, repeat=4):
     elif b"Wrong" in response:
         continue
     elif b"Time" in response:
-        print(''.join(candidate))
+        print("".join(candidate))
         print(response)
         break
-    sleep(1e-3) #Don't overload the server
+    sleep(1e-3)  # Don't overload the server
